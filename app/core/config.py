4181b91aacb7456ff348afbcf65cd2ffb709b299
PIZANO DURAN JORGE ARTURO <jorge.pizano@ine.mx>
2026-03-30 22:23:51 -0600

    fix: cambia driver a psycopg2

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,6 @@ class Settings:
     def DATABASE_URL(self):
         return (
             f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}"
-            
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
 
